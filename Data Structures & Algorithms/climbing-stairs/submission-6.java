adbb3ea96ffb263ef625b3668c5e2a644d7375b0
imaadsyed <40012625+imaadsyed@users.noreply.github.com>
2026-04-24 19:14:46 -0700

    Add: climbing-stairs - submission-6

---
+++ Data Structures & Algorithms/climbing-stairs/submission-6.java
@@ -0,0 +1,14 @@
+class Solution {
+    private int[] memo = new int[46];
+
+    public int climbStairs(int n) {
+        if (n <= 2) {
+            return n;
+        }
+        if (memo[n] != 0) {
+            return memo[n];
+        }
+
+        return memo[n] = climbStairs(n - 1) + climbStairs(n - 2);
+    }
+}
