8207a845d66b7e971d84b34419efb5cb5a753f05
imaadsyed <40012625+imaadsyed@users.noreply.github.com>
2026-05-05 00:32:52 -0700

    Add: continuous-subarray-sum - submission-2

---
+++ Data Structures & Algorithms/continuous-subarray-sum/submission-2.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean checkSubarraySum(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, -1);
+        int runningSum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            runningSum = (runningSum + nums[i]) % k;
+            int remainder = runningSum;
+            if (map.containsKey(remainder)) {
+                if (i - map.get(remainder) >= 2) {
+                    return true;
+                }
+            } else {
+                map.put(remainder, i);
+            }
+        }
+        return false;
+    }
+}
