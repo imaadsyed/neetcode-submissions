3be5aa51eedf2001b480e22a9f854adb3f47fa57
imaadsyed <40012625+imaadsyed@users.noreply.github.com>
2026-05-05 20:38:50 -0700

    Add: minimum-size-subarray-sum - submission-3

---
+++ Data Structures & Algorithms/minimum-size-subarray-sum/submission-3.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int minSubArrayLen(int target, int[] nums) {
+        int l = 0;
+        int minWindow = 100001;
+        int windowSum = 0;
+        for (int r = 0; r < nums.length; r++) {
+            windowSum += nums[r];
+            while (windowSum >= target) {
+                minWindow = Math.min(minWindow, r - l + 1);
+                windowSum -= nums[l];
+                l++;
+            }
+        }
+
+        return minWindow == 100001 ? 0 : minWindow;
+    }
+}
+
+// Time Complexity: O(n)
+// Space Complexity: O(1)
