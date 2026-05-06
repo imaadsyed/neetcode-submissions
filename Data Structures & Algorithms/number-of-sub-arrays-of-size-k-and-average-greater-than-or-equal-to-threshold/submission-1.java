0a6b174eb8e4a04e54ea52ddac1a3d3d0b9bc508
imaadsyed <40012625+imaadsyed@users.noreply.github.com>
2026-05-05 20:03:03 -0700

    Add: number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold - submission-1

---
+++ Data Structures & Algorithms/number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/submission-1.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int numOfSubarrays(int[] arr, int k, int threshold) {
+        Queue<Integer> window = new LinkedList<>();
+        int count = 0;
+        int l = 0;
+        int windowSum = 0;
+        for (int r = 0; r < arr.length; r++) {
+            windowSum += arr[r];
+            window.offer(arr[r]);
+
+            if (window.size() == k) {
+                if (windowSum / k >= threshold) {
+                    count++;
+                }
+                // System.out.println(window + " avg: " + windowSum/k + " count: " + count);
+                // pop from queue – increase window size
+                windowSum -= window.poll();
+                l++;
+            }
+        }    
+        return count;
+    }
+
+    // Time complexity: O(n)
+    // Space complexity = O(k)
+}
