6d3a03dc727b3e29e4fd3a9577132479df5358ab
imaadsyed <40012625+imaadsyed@users.noreply.github.com>
2026-05-16 16:16:10 -0700

    Add: maximum-twin-sum-of-a-linked-list - submission-1

---
+++ Data Structures & Algorithms/maximum-twin-sum-of-a-linked-list/submission-1.java
@@ -0,0 +1,41 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public int pairSum(ListNode head) {
+        ListNode left = head;
+        ListNode right = head;
+        Stack<ListNode> stack = new Stack<>();
+        
+        while(right.next != null) {
+            stack.add(right);
+            right = right.next;
+        }
+
+        int i = 0;
+        int max = 0;
+        while (!stack.isEmpty()) {
+            int sum = left.val + right.val;
+
+            if (sum > max) {
+                max = sum; 
+            }
+
+            left = left.next;
+            right = stack.pop();
+        }
+
+        return max;
+    }
+}
+
+
+// Time complexity = O(n)
+// Space complexity = O(n)
