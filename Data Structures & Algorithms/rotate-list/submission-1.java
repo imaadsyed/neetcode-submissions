b6fc3aa7c8230ce51246319a18fc61a6de61aadd
imaadsyed <40012625+imaadsyed@users.noreply.github.com>
2026-05-17 22:07:40 -0700

    Add: rotate-list - submission-1

---
+++ Data Structures & Algorithms/rotate-list/submission-1.java
@@ -0,0 +1,63 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode rotateRight(ListNode head, int k) {
+        // k - 1.next = null
+        // k last element = head
+        // tail.next = head
+        int length = getListLength(head);
+        if (head == null) {
+            return null;
+        }
+
+        k = k % length;
+        if (k == 0) {
+            return head;
+        }
+
+        k = length - k;
+
+        ListNode newTail = getNodeAt(head, k - 1);
+        ListNode newHead = getNodeAt(head, k);
+        ListNode tail = getLastNode(head);
+
+        newTail.next = null;
+        tail.next = head;
+        
+        return newHead;
+    }
+
+    private int getListLength(ListNode head) {
+        if (head == null) {
+            return 0;
+        }
+        if (head.next == null) {
+            return 1;
+        }
+        return 1 + getListLength(head.next);
+    }
+
+    private ListNode getNodeAt(ListNode head, int k) {
+        if (k == 0 || head.next == null) {
+            return head;
+        }
+
+        return getNodeAt(head.next, k-1);
+    }
+
+    private ListNode getLastNode(ListNode head) {
+        if (head.next == null) {
+            return head;
+        }
+
+        return getLastNode(head.next);
+    }
+}
